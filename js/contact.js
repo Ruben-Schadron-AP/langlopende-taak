6f3ec12be785dbb1419181e6b8d5edd720525a4d
Ruben Schadron <schadronruben@hotmail.com>
2022-12-17 03:10:22 +0100

    added dark light mode

--- js/contact.js
+++ js/contact.js
@@ -1,9 +1,9 @@
-let cmap = L.map('cmap').setView([51.23009, 4.41616], 17);
+let kmap = L.map('kmap').setView([51.23009, 4.41616], 17);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(cmap);
+}).addTo(kmap);
 
-let marker = L.marker([51.23009, 4.41616]).addTo(cmap);
+let marker = L.marker([51.23009, 4.41616]).addTo(kmap);
 marker.bindPopup('<p class="fw-bolder">Kantoor</p> <p>Ellermanstraat 33</p>').openPopup();
